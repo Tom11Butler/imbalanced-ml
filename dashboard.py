5a7368b16807612b72b3830856f10983a6132721
Tom Butler <tombutler21@hotmail.co.uk>
2022-07-12 09:24:55 +0100

    Updated the dashboard to show model scores and highlight positives.

--- dashboard.py
+++ dashboard.py
@@ -17,6 +17,13 @@ sns.set_style('whitegrid')
 st.set_page_config(layout="wide")
 
 
+def highlight(df):
+    if df['true'] == 1:
+        return ['background-color: lightgreen'] * len(df)
+    else:
+        return ['background-color: white'] * len(df)
+
+
 class Dashboard:
     def __init__(self):
         self.data_scores = None
@@ -28,7 +35,7 @@ class Dashboard:
         self.title()
         self.sidebar()
         self.plot_scores()
-        self.metrics()
+        # self.metrics()
 
     def title(self):
         st.title("Imbalanced Training Data")
@@ -59,10 +66,13 @@ class Dashboard:
 
     def plot_scores(self):
         # TODO matplotlib plot of the scores for a given model
+
+        col1, col2 = st.columns([3,1])
+
         df_scores = pd.DataFrame(self.data_scores[f'{self.ratio}'], columns=['proba'])
         df_scores = pd.merge(df_scores, self.data_true, left_index=True, right_index=True)
 
-        fig = plt.figure(figsize=(10, 5))
+        fig = plt.figure(figsize=(20, 5))
         plt.title(
             f"Histogram of scores for a training class ratio of {self.ratio}",
             fontdict={"fontsize": 16}
@@ -80,11 +90,18 @@ class Dashboard:
             alpha=0.7
         )
         plt.legend()
-        st.pyplot(fig)
+
+        df_scores = df_scores.sort_values("proba", ascending=False).reset_index(drop=True)
+        # df_scores = df_scores.style.apply(highlight, axis=1)
+
+        with col1:
+            st.pyplot(fig)
+        with col2:
+            st.dataframe(df_scores.style.apply(highlight, axis=1))
 
     def metrics(self):
         st.markdown("## Metrics for the different ratios")
-        fig = plt.figure(figsize=(10, 5))
+        fig = plt.figure(figsize=(20, 5))
         plt.plot(self.data_metrics["model_ratio"], self.data_metrics["accuracy"], label="Accuracy")
         plt.plot(self.data_metrics["model_ratio"], self.data_metrics["precision"], label="Precision")
         plt.plot(self.data_metrics["model_ratio"], self.data_metrics["recall"], label="Recall")
@@ -93,7 +110,6 @@ class Dashboard:
         )
         plt.legend()
         st.pyplot(fig)
-
 
 if __name__ == "__main__":
     dashboard = Dashboard()
